23cfbb56e6854751d4974d8605760f12d2ddb483
Evgeny Mandrikov <mandrikov@gmail.com>
2010-10-29 22:36:19 +0000

    SONARPLUGINS-760: Use sonar-plugin packaging, upgrade to parent 6, fix license headers

--- src/test/java/org/sonar/plugins/buildbreaker/BuildBreakerPluginTest.java
+++ src/test/java/org/sonar/plugins/buildbreaker/BuildBreakerPluginTest.java
@@ -1,6 +1,6 @@
 /*
  * Sonar, open source software quality management tool.
- * Copyright (C) 2009 SonarSource SA
+ * Copyright (C) 2009 SonarSource
  * mailto:contact AT sonarsource DOT com
  *
  * Sonar is free software; you can redistribute it and/or
@@ -17,6 +17,7 @@
  * License along with Sonar; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
+
 package org.sonar.plugins.buildbreaker;
 
 import static org.hamcrest.CoreMatchers.is;
